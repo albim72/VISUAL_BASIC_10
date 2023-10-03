b8452a8373572fd5009f33c866415f47e3dc8e82
albim72 <marcin.albiniak72@gmail.com>
2023-10-03 09:51:06 +0200

    Create module1.vb

---
+++ DZIEN_2/klasa123/module1.vb
@@ -0,0 +1,12 @@
+Module Module1
+
+    Sub Main()
+        Dim pr As PierwszaKlasa = New PierwszaKlasa(5, 11)
+
+        Console.WriteLine("____ PierwszaKlasa______")
+        pr.printAB()
+        pr.Oblicz()
+
+    End Sub
+
+End Module
