db78fabbb041bcccbffbd45792db8a20b91073b6
albim72 <marcin.albiniak72@gmail.com>
2023-10-03 11:20:53 +0200

    Update Module1.vb

--- DZIEN_2/Figura/Module1.vb
+++ DZIEN_2/Figura/Module1.vb
@@ -9,7 +9,25 @@ Module Module1
         pr.Info(s1)
         Console.WriteLine("pole wynosi = {0}", pr.policzPole)
 
+
+        Dim tr As Trojkat = New Trojkat(6, 8.8)
+        Dim s2 = "FIGURA TRÓJKĄT"
+        pr.Info(s2)
+        Console.WriteLine("pole wynosi = {0}", tr.policzPole)
+
+        tr = New Trojkat(111, 333)
+        Dim s44 = "FIGURA TRÓJKĄT"
+        pr.Info(s44)
+        Console.WriteLine("pole wynosi = {0}", tr.policzPole)
+
+        Dim trp As Trapez = New Trapez(6.6, 7.2, 4.8)
+        Dim s3 = "FIGURA TRAPEZ"
+        pr.Info(s3)
+        Console.WriteLine("pole wynosi = {0}", trp.policzPole)
+
         Console.ReadKey()
+
+
 
 
     End Sub
