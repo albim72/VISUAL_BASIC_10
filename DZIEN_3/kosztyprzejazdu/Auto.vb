c85c03adac471a68c35754d031e088f1e8728d6b
albim72 <marcin.albiniak72@gmail.com>
2023-10-04 11:39:03 +0200

    Update Auto.vb

--- DZIEN_3/kosztyprzejazdu/Auto.vb
+++ DZIEN_3/kosztyprzejazdu/Auto.vb
@@ -1,11 +1,9 @@
 Public MustInherit Class Auto
     Public odleglosc As Double
-    Public jednostki As Double
     Public cenaj As Double
 
-    Protected Sub New(odleglosc As Double, jednostki As Double, cenaj As Double)
+    Protected Sub New(odleglosc As Double, cenaj As Double)
         Me.odleglosc = odleglosc
-        Me.jednostki = jednostki
         Me.cenaj = cenaj
     End Sub
 
