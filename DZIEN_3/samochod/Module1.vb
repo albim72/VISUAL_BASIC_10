3f93f424e4da3e4e203d2c775f210fc301ed003b
albim72 <marcin.albiniak72@gmail.com>
2023-10-04 09:20:07 +0200

    Create Module1.vb

---
+++ DZIEN_3/samochod/Module1.vb
@@ -0,0 +1,23 @@
+Module Module1
+
+    Sub Main()
+
+        Dim cars As New List(Of Car) From
+            {
+            New Car With {.Name = "Audi", .Color = "blue", .Speed = 230},
+            New Car With {.Name = "Seat", .Color = "red", .Speed = 210},
+            New Car With {.Name = "BMW", .Color = "green", .Speed = 250},
+            New Car With {.Name = "Opel", .Color = "blue", .Speed = 210},
+            New Car With {.Name = "Mercedes", .Color = "blue", .Speed = 280},
+            New Car With {.Name = "Toyota", .Color = "red", .Speed = 230},
+            New Car With {.Name = "KIA", .Color = "green", .Speed = 220}
+        }
+
+        cars.Sort()
+
+        For Each
+
+
+    End Sub
+
+End Module
