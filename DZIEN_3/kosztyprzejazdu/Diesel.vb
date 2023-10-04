4fe6066489176246d9abf12e23bb5c6313dd37db
albim72 <marcin.albiniak72@gmail.com>
2023-10-04 11:31:17 +0200

    Create Diesel.vb

---
+++ DZIEN_3/kosztyprzejazdu/Diesel.vb
@@ -0,0 +1,13 @@
+Public Class Diesel : Inherits Auto
+
+    Public Sub New(odleglosc As Double, jednostki As Double, cenaj As Double)
+        MyBase.New(odleglosc, jednostki, cenaj)
+    End Sub
+    Dim avg_jedn As Double = 26.6
+    Dim avg_odl As Double = 300
+
+    Dim spal100 = avg_jedn * 100 / avg_odl
+    Public Overrides Function policzKosztPaliwa() As Double
+        Return spal100 * odleglosc * cenaj / 100
+    End Function
+End Class
