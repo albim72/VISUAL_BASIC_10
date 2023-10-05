ae503e9812147559950a9d15b90e287a86f7a92b
albim72 <marcin.albiniak72@gmail.com>
2023-10-05 09:26:13 +0200

    Update form1.vb

--- DZIEN_4/dodajRekoredOkno/form1.vb
+++ DZIEN_4/dodajRekoredOkno/form1.vb
@@ -6,6 +6,29 @@ Public Class Form1
     Public db As New DataClasses1DataContext(connectionStr)
 
     Private Sub btnDodaj_Click(sender As Object, e As EventArgs) Handles btnDodaj.Click
+        Dim idd, name
+        Dim price As Double
+
+        idd = tbID.Text
+        name = tbNazwa.Text
+        price = Double.Parse(tbCena.Text)
+
+        Dim nowyProd As New Produkty()
+
+        nowyProd.IDProduktu = idd
+        nowyProd.NazwaProduktu = name
+        nowyProd.CenaJednostkowa = price
+
+        db.Produkty.InsertOnSubmit(nowyProd)
+        db.SubmitChanges()
+
+        Dim opis = "Rekord został dodany id = " & nowyProd.IDProduktu
+
+        MessageBox.Show(opis, "informacja")
+
+        tbID.Clear()
+        tbNazwa.Clear()
+        tbCena.Clear()
 
     End Sub
 End Class
